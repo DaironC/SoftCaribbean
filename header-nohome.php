29ac5d409ac8c4eb5d1f07416dfa4e0062bea583
Juan David <jcobos1999@gmail.com>
2018-09-06 13:52:17 -0500

    margin en logo

--- header-nohome.php
+++ header-nohome.php
@@ -24,12 +24,12 @@
                 </div>    
                 </nav>
          <!-- Logo -->
-         <?php if ( get_header_image() ) : ?>
-         <div id="site-header">
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-            <img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-            </a>
-         </div>
+         <?php if (get_header_image()) : ?>
+                    <div id="site-header">
+                        <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+                            <img id="logo" src="<?php header_image(); ?>" width="<?php echo absint(get_custom_header()->width); ?>" height="<?php echo absint(get_custom_header()->height); ?>" alt="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
+                        </a>
+                    </div>
          <?php endif; ?>
          <h1><a href="<?php home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
          <?php  
