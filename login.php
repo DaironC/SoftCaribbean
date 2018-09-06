a98807911516ba58359f8cb6fd813fdbedea9256
Dairon <dc1094@hotmail.com>
2018-09-06 13:02:30 -0500

    login.php

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  Template Name: Login
+ */
+?>
+
+<?php get_header('nohome'); ?>
+
+
+<!-- Main -->
+<main id="main">
+    <div class="container">
+     <h1>Sofca</h1>
+    </div>
+</main>
+
+  <?php get_footer();?>
+      
