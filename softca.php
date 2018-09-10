09bc24df029ed8ac20dd199562cd34630052e0ed
Juan David <jcobos1999@gmail.com>
2018-09-10 14:05:28 -0500

    navpanel intranet

--- softca.php
+++ softca.php
@@ -3,59 +3,13 @@
   Template Name: intranet
  */
 ?>
-<?php get_header('intranet');?><!--header de intranet-->
-     
+<?php get_header('intranet'); ?>  
 
 <!-- Main -->
 <main id="main">
-   <div class="container">
-   <div class="row">
-      <div class="col-4">
-         <aside class="especiales">
-            <h4>Fechas Especiales</h4>
-            <p>The Epcot Center is a theme park in Disney World, Florida.</p>
-         </aside>
-      </div>
-    
-     
-      <div class="col-8">
-         <section class="logos">
-         
-         <p>ssssssssssssss</p>
-         
-         </section>
-         <section class="logos">
-         
-         <p>ssssssssssssss</p>
-         
-         </section>
-         </div>
-      </div>
-   </div>
-
-
-  <div class="container">
-    <div class="row">
-
-      <div class="col-4">
-        <h4>Reconocimiento 1</h4>
-        <p></p>
-      </div>
-    
-      <div class="col-4">
-      <h4>Reconocimiento 2</h4>
-      </div>
-
-      <div class="col-4">
-      <h4>Reconocimiento 3</h4>
-      </div>
-
+    <div class="container">
+     <h3>Intranet de SoftCaribbean</h1>
     </div>
-  </div>
 </main>
 
-<footer >
-  <div id="intra" >
-    <a> Softcaribbean 2018. Todos Los Derechos Reservados.</a>
-  </div>
-</footer>
+<?php get_footer();?>
