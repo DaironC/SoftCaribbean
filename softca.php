a61debf1d06b248cfa7bf8c622c154b9f84aaa9f
Juan David <jcobos1999@gmail.com>
2018-09-10 15:48:41 -0500

    cumpleaños

--- softca.php
+++ softca.php
@@ -10,7 +10,7 @@
       <div class="row">
          <div class="col-4">
             <aside class="especiales">
-               <h4>Fechas Especiales</h4>
+               <h4 class="fechas-especiales">Fechas Especiales</h4>
                <div class="card text-center">
                   <div class="card-header">
                     Feliz Cumpleaños
@@ -25,6 +25,23 @@
                      10 de Septiembre del 2018
                   </div>
                </div>
+
+            <div class="card text-center">
+                  <div class="card-header">
+                    Feliz Cumpleaños
+                  </div>
+                  <div class="card-body">
+                     <h5 class="card-title">Pepito Perez</h5>
+                     <img class="icono-login" src="<?php bloginfo('template_directory') ?>/images/img_avatar.png" >
+                     <p class="card-text">Softcaribbean te desa un feliz cumpelaños y que mejores ya que te necistamos para crecer cada día más </p>
+                   
+                  </div>
+                  <div class="card-footer text-muted">
+                     10 de Septiembre del 2018
+                  </div>
+               </div>
+
+
             </aside>
          </div>
          <div class="col-8">
