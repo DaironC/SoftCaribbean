ef5391466500c5bf9f2bd9efc9587991e2773c83
Dairon <dc1094@hotmail.com>
2018-09-10 14:20:35 -0500

    softca

---
+++ footer-intranet.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+
+<div id="intra" >
+    <a> Softcaribbean 2018. Todos Los Derechos Reservados.</a>
+  </div>
+<?php wp_footer(); ?>
+        <!--[if lte IE 8]><script src="<?php bloginfo('stylesheet_directory'); ?>/assets/js/ie/respond.min.js"></script><![endif]-->
+
+
+</body>
+</html>
--- softca.php
+++ softca.php
@@ -8,8 +8,55 @@
 <!-- Main -->
 <main id="main">
     <div class="container">
-     <h3>Intranet de SoftCaribbean</h1>
+
+         <div class="row">
+      <div class="col-4">
+         <aside class="especiales">
+            <h4>Fechas Especiales</h4>
+            <p>The Epcot Center is a theme park in Disney World, Florida.</p>
+         </aside>
+      </div>
+    
+     
+      <div class="col-8">
+         <section class="logos">
+         
+         <p>ssssssssssssss</p>
+         
+         </section>
+         <section class="logos">
+         
+         <p>ssssssssssssss</p>
+         
+         </section>
+         </div>
+      </div>
+   </div>
+
+
+  <div class="container">
+    <div class="row">
+
+      <div class="col-4">
+        <h4>Reconocimiento 1</h4>
+        <p></p>
+      </div>
+    
+      <div class="col-4">
+      <h4>Reconocimiento 2</h4>
+      </div>
+
+      <div class="col-4">
+      <h4>Reconocimiento 3</h4>
+      </div>
+
+    </div>
+  </div>
+
+
+
+
     </div>
 </main>
 
-<?php get_footer();?>
+<?php get_footer('intranet');?>
