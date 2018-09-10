904eb8a1127b38d31612ea68191dbb5b0a30902b
Harwin Silva <hasd35@gmail.com>
2018-09-10 14:15:29 -0500

    Aplicar convocatoria

---
+++ aplicar-convocatoria.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  Template Name: intranet
+ */
+?>
+<?php get_header(); ?>  
+
+<!-- Main -->
+<main id="main">
+    <div class="container">
+     <h3>Enviar hoja de Vida</h1>
+    </div>
+</main>
+
+<?php get_footer();?>
