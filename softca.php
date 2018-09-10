438668ad93a64e1442bc5c9d5cb7fc0e0f778f75
Harwin Silva <hasd35@gmail.com>
2018-09-10 16:36:26 -0500

    update

--- softca.php
+++ softca.php
@@ -6,7 +6,6 @@
 <?php get_header('intranet'); ?>  
 <!-- Main -->
 <main id="main">
-<<<<<<< HEAD
     <div class="container">
 
          <div class="row">
@@ -30,27 +29,6 @@
          </div>
       </div>
    </div>
-=======
-   <div class="container">
-      <div class="row">
-         <div class="col-4">
-            <aside class="especiales">
-               <h4 class="fechas-especiales">Fechas Especiales</h4>
-               <div class="card text-center">
-                  <div class="card-header">
-                    Feliz Cumpleaños
-                  </div>
-                  <div class="card-body">
-                     <h5 class="card-title">Pepito Perez</h5>
-                     <img class="icono-login" src="<?php bloginfo('template_directory') ?>/images/img_avatar.png" >
-                     <p class="card-text">Softcaribbean te desa un feliz cumpelaños y que mejores ya que te necistamos para crecer cada día más </p>
-                   
-                  </div>
-                  <div class="card-footer text-muted">
-                     10 de Septiembre del 2018
-                  </div>
-               </div>
->>>>>>> a61debf1d06b248cfa7bf8c622c154b9f84aaa9f
 
             <div class="card text-center">
                   <div class="card-header">
