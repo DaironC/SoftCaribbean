5d0fbb606e48fdef180ab5a8c99fe8c4d408ab1c
Harwin Silva <hasd35@gmail.com>
2018-09-11 08:00:57 -0500

    aplicar convocatoria

--- aplicar-convocatoria.php
+++ aplicar-convocatoria.php
@@ -8,7 +8,27 @@
 <!-- Main -->
 <main id="main">
     <div class="container">
-     <h3>Enviar hoja de Vida</h1>
+    <form action="">
+         <div class="card-enviarHV">
+         <div style="text-align: center;"><h2>Adjuntar Hoja de Vida</h1></div>
+         <div class="row">
+            <div class="col-md-4">
+            <label for="nombre">¿Cómo te llamas?</label>
+            <input class="nombre" id="nombre" type="text" name="nombre" placeholder="Nombre" required>
+            <label for="temas">¿Cuales son tus temas de conocimiento?</label>
+            <textarea class="temas" id="temas" rows="2" type="text" name="temas" placeholder="Temas"></textarea>
+            </div>
+            <div class="col-md-4">
+            <label for="numero">¿Cúal es tu número telefónico?</label>
+            <input class="telefono" id="numero"type="text" name="telefono" placeholder="Telefono" required>
+            </div>
+            <div class="col-md-4">
+            <label for="email">¿Cúal es tu correo electrónico?</label>
+            <input class="email" id="email" type="text" name="email" placeholder="e-mail" required>
+            </div>
+        </div>
+         </div>
+      </form>
     </div>
 </main>
 
