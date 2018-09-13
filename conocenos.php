2af430113e332f1e82e11d599303a2979a9d3115
Dairon <dc1094@hotmail.com>
2018-09-13 17:06:14 -0500

    conocenos

--- conocenos.php
+++ conocenos.php
@@ -17,7 +17,7 @@
       <div class="card">
          <img class="card-img-top"  style="width:150px;height:80px;"  src="<?php bloginfo('template_directory') ?>/images/DAIRON.jpg">
          <button class=" btn-miembros " type="button" data-toggle="collapse" data-target="#collapsefive" aria-expanded="false" aria-controls="collapsefive">
-         Juan Cardenas
+         Dairon Correa
          </button>
          <div id="collapsefive" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
             <div class="card-body">
@@ -33,9 +33,9 @@
 
       <div class="col-md-3">
       <div class="card">
-         <img class="card-img-top"  style="width:150px;height:80px;"  src="<?php bloginfo('template_directory') ?>/images/DAIRON.jpg">
+         <img class="card-img-top"  style="width:150px;height:80px;"  src="<?php bloginfo('template_directory') ?>/images/cobos.PNG">
          <button class=" btn-miembros " type="button" data-toggle="collapse" data-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
-         Juan Cardenas
+         Juan Cobos
          </button>
          <div id="collapse2" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
             <div class="card-body">
@@ -50,9 +50,9 @@
 
       <div class="col-md-3">
       <div class="card">
-         <img class="card-img-top"  style="width:150px;height:80px;"  src="<?php bloginfo('template_directory') ?>/images/DAIRON.jpg">
+         <img class="card-img-top"  style="width:150px;height:80px;"  src="<?php bloginfo('template_directory') ?>/images/dani.jpg">
          <button class=" btn-miembros " type="button" data-toggle="collapse" data-target="#collapsetwo" aria-expanded="false" aria-controls="collapsetwo">
-         Juan Cardenas
+         Daniela Lopera
          </button>
          <div id="collapsetwo" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
             <div class="card-body">
@@ -67,9 +67,9 @@
 
       <div class="col-md-3">
       <div class="card">
-         <img class="card-img-top"  style="width:150px;height:80px;"  src="<?php bloginfo('template_directory') ?>/images/DAIRON.jpg">
+         <img class="card-img-top"  style="width:150px;height:80px;"  src="<?php bloginfo('template_directory') ?>/images/harwin.PNG">
          <button class=" btn-miembros " type="button" data-toggle="collapse" data-target="#collapseone" aria-expanded="false" aria-controls="collapseone">
-         Juan Cardenas
+         harwin
          </button>
          <div id="collapseone" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
             <div class="card-body">
